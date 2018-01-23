a4cf8f77db1ff192d702beb5007990c575dff89c
Daniel Snider <danielsnider12@gmail.com>
2018-01-22 22:14:17 -0500

    cleanup

--- plugins/spotting/spotB/definition_spotB.m
+++ plugins/spotting/spotB/definition_spotB.m
@@ -9,10 +9,3 @@ params(2).default = 2;
 params(2).help = 'eueuiouiouieoi oeuiouio ';
 params(2).type = 'Numeric';
 
-
-% names = {'Ann'; 'Ramesh'; 'Al'; 'Kim'; 'Mary'; 'Sid'; 'Don'};
-% height = num2cell([1.3; 1.4; 1.5; 1.6; 1.8; 1.9; 2.05;]);
-% weight = num2cell([49;  60;  49; 103; 68; 138; 150]);
-% show = num2cell(true(7,1));
-% app.UITable.Data = [names height weight show];
-% app.UITable.Position = [160 30 320 150];
