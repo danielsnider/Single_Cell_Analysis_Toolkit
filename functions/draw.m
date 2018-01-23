426e819e55325798ecf9209f299ab11843d28ab1
Daniel Snider <danielsnider12@gmail.com>
2018-01-22 21:56:08 -0500

    almost

---
+++ functions/draw.m
@@ -0,0 +1,29 @@
+function fun(app, params, createCallbackFcn)
+% function fun(algo_name, params, app, createCallbackFcn)
+  % Setup a string list of dynamic arguments to be passed to the plugin.
+  % for example:
+  %    'app.spotting.fields{1}.Value, app.spotting.fields{2}.Value'
+  app_params = {};
+  for idx=1:length(params)
+    app_params(idx) = {sprintf('app.spotting.fields{%s}.Value', num2str(idx))};
+  end
+  app_params = strjoin(app_params,', ');
+  v_offset = 100
+  for idx=1:length(params)
+    v_offset = v_offset + 50
+    field_pos = [165 v_offset 50 22];
+    label_pos = [5 v_offset-5 145 22];
+
+    fieldCallback = @(app, event) eval(['spotA(app.img, ' app_params ');']);
+
+    app.spotting.fields{idx} = uispinner(app.SpottingTab);
+    app.spotting.fields{idx}.ValueChangedFcn = createCallbackFcn(app, fieldCallback, true);
+    app.spotting.fields{idx}.Position = field_pos;
+    app.spotting.fields{idx}.Value = params(idx).default;
+    
+    app.spotting.labels{idx} = uilabel(app.SpottingTab);
+    app.spotting.labels{idx}.HorizontalAlignment = 'right';
+    app.spotting.labels{idx}.Position = label_pos;
+    app.spotting.labels{idx}.Text = params(idx).name;
+  end
+end
--- plugins/spotting/spotB/spotB.m
+++ plugins/spotting/spotB/spotB.m
@@ -1,12 +1,13 @@
-function [X Y] = spotA(img, smooth_param)
-  img_smooth = imgaussfilt(img,12); % more smoothing to join close seeds
+function [X Y] = spotA(img, smooth_param, markersize)
+  img_smooth = imgaussfilt(img,smooth_param); % more smoothing to join close seeds
   seeds = imregionalmax(img_smooth);
-  seeds(img_mask==0)=0; % remove seeds outside of our img mask
+  % seeds(img_mask==0)=0; % remove seeds outside of our img mask
   % Debug with plot
-  [X Y] = find(seeds)
-  figure('name','seeds','NumberTitle', 'off')
+  [X Y] = find(seeds);
+  %figure('name','seeds','NumberTitle', 'off')
+  figure(2)
   imshow(img,[]);
   hold on;
-  plot(Y,X,'or','markersize',2,'markerfacecolor','r')
+  plot(Y,X,'or','markersize',markersize*2,'markerfacecolor','r')
 
 end
