5c5d1e0599aa9ce5a18aeecf3fd9a2f4355bd62d
Justin Sing <justincsing@gmail.com>
2018-02-20 13:00:56 -0500

    Cell_Cycle_Length

---
+++ plugins/analyze/Cell_Cycle_Calculation/Cell_Cycle.m
@@ -0,0 +1,31 @@
+function fun(plugin_name, plugin_num, x, y, marker_size, title_param, fontsize_param, trend_line, correlation_type)
+
+  f = figure(plugin_num+344); clf; set(f,'name',plugin_name,'NumberTitle', 'off');
+
+  plot(x.data,y.data, 'o', 'Color', [.6 .6 .6],'MarkerSize', marker_size,'MarkerFaceColor',[.6 .6 .6],'MarkerEdgeColor','w');
+  
+  set(gca,'FontSize',fontsize_param);
+  set(gca,'Color',[1 1 1 ]);
+  set(gcf,'Color',[1 1 1 ]);
+
+  xlabel(x.label, 'Interpreter','none');
+  ylabel(y.label, 'Interpreter','none');
+
+  box off
+
+  if trend_line
+    lsline
+  end
+
+  if ~isequal(correlation_type,false)
+    [r,p] = corr(x.data,y.data,'type',correlation_type);
+    if isempty(title_param)
+      title_param = sprintf('r=%.2f, p=%.2f',r,p);
+    else
+      title_param = sprintf('%s (r=%.2f, p=%.2f)',title_param,r,p);
+    end
+  end
+
+  title(title_param);
+
+end
---
+++ plugins/analyze/Cell_Cycle_Calculation/definition_Cell_Cycle.m
@@ -0,0 +1,55 @@
+function [params, algorithm] = fun()
+
+  algorithm.name = 'Cell Cycle';
+  algorithm.help = 'Calculate Cell Cycle Length using Least Linear Squares Fitting.';
+  algorithm.image = 'Cell_Cycle_Duration.gif';
+  algorithm.maintainer = 'Daniel Snider <danielsnider12@gmail.com>';
+
+  n = 0;
+  n = n + 1;
+  params(n).name = 'Data X-axis';
+  params(n).default = '';
+  params(n).help = 'Choose a measure that will be used on the X-axis.';
+  params(n).type = 'measurement_dropdown';
+% 
+%   n = n + 1;
+%   params(n).name = 'Data Y-axis';
+%   params(n).default = '';
+%   params(n).help = 'Choose a measure that will be used on the X-axis.';
+%   params(n).type = 'measurement_dropdown';
+% 
+%   n = n + 1;
+%   params(n).name = 'Marker Size';
+%   params(n).default = 7;
+%   params(n).help = 'Set the size of the points plotted.';
+%   params(n).type = 'numeric';
+%   params(n).limits = [0 Inf];
+% 
+%   n = n + 1;
+%   params(n).name = 'Figure Title';
+%   params(n).default = '';
+%   params(n).help = 'Sets the title of the figure to this parameter.';
+%   params(n).type = 'text';
+% 
+%   n = n + 1;
+%   params(n).name = 'Font Size';
+%   params(n).default = 14;
+%   params(n).help = 'Sets the font size for the title and axis labels.';
+%   params(n).type = 'numeric';
+% 
+%   n = n + 1;
+%   params(n).name = 'Trend Line';
+%   params(n).default = true;
+%   params(n).help = 'Adds a trend line.';
+%   params(n).type = 'checkbox';
+% 
+%   n = n + 1;
+%   params(n).name = 'Display Correlation';
+%   params(n).default = 'Pearson';
+%   params(n).help = 'Adds the p-value and correlation coefficient it the figure title.';
+%   params(n).type = 'dropdown';
+%   params(n).options = {'Pearson', 'Kendall', 'Spearman'};
+%   params(n).optional = true;
+
+
+end
